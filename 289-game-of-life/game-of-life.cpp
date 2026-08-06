3de32b1d41f6d0511a42e891d0c75e1109695ada
Dishank Gandhi <dishankgandhi561@gmail.com>
2026-08-07 04:07:03 +0530

    Time: 0 ms (100.00%) | Memory: 9.6 MB (33.38%) - LeetSync

--- 289-game-of-life/game-of-life.cpp
+++ 289-game-of-life/game-of-life.cpp
@@ -4,8 +4,6 @@ public:
         int m = board.size();
         int n = board[0].size();
 
-        vector<vector<int>> nextboard = board;
-
         for(int i=0 ; i<m ; i++)
         {
             for(int j=0 ; j<n ; j++)
@@ -13,23 +11,32 @@ public:
                 int count = 0;
                 if(j-1 >= 0)
                 {
-                    if(i-1 >= 0 && nextboard[i-1][j-1] == 1) count++;
-                    if(i+1 < m && nextboard[i+1][j-1] == 1) count++;
-                    if(nextboard[i][j-1]) count++;
+                    if(i-1 >= 0 && (board[i-1][j-1] == 1 || board[i-1][j-1] == 3)) count++;
+                    if(i+1 < m && (board[i+1][j-1] == 1 || board[i+1][j-1] == 3)) count++;
+                    if(board[i][j-1] == 1|| board[i][j-1] == 3) count++;
                 }
 
                 if(j+1 < n)
                 {
-                    if(i-1 >= 0 && nextboard[i-1][j+1] == 1) count++;
-                    if(i+1 < m && nextboard[i+1][j+1] == 1) count++;
-                    if(nextboard[i][j+1]) count++;
+                    if(i-1 >= 0 && (board[i-1][j+1] == 1 || board[i-1][j+1] == 3)) count++;
+                    if(i+1 < m && (board[i+1][j+1] == 1 || board[i+1][j+1] == 3)) count++;
+                    if(board[i][j+1] == 1 || board[i][j+1] == 3) count++;
                 }
 
-                if(i-1 >= 0 && nextboard[i-1][j] == 1) count++;
-                if(i+1 < m && nextboard[i+1][j] == 1) count++;
+                if(i-1 >= 0 && (board[i-1][j] == 1 || board[i-1][j] == 3)) count++;
+                if(i+1 < m && (board[i+1][j] == 1 || board[i+1][j] == 3)) count++;
 
-                if(nextboard[i][j] == 0 && count == 3) board[i][j] = 1;
-                if(nextboard[i][j] == 1 && (count != 2 && count != 3)) board[i][j] = 0;
+                if(board[i][j] == 0 && count == 3) board[i][j] = 2;
+                if(board[i][j] == 1 && (count < 2 || count > 3)) board[i][j] = 3;
+            }
+        }
+
+        for(int i=0 ; i<m ; i++)
+        {
+            for(int j=0 ; j<n ; j++)
+            {
+                if(board[i][j] == 2) board[i][j] = 1;
+                if(board[i][j] == 3) board[i][j] = 0;
             }
         }
     }
